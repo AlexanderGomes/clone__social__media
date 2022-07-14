fed40f6572896beabc5a435a566e155fc90a85d0
Alexsander Gomes baptista <sander@Alexsanders-MacBook-Pro.local>
2022-07-14 13:15:42 -0700

    done

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -22,8 +22,8 @@ const updatePost = asyncHandler(async (req, res) => {
             await post.updateOne({$set: req.body});
             res.status(200).json({post, message: 'post has been updated'})
         }
-    } catch (error) {
-        res.status(400).json(error.message)
+    } catch (err) {
+        res.status(400).json(err.message)
     }
 })
 
