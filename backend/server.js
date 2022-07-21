91d97d834679e934e7d09a5cb159c6e1b5cf0967
Alexsander Gomes baptista <gomes.alexsander.hamir@outlook.com>
2022-07-21 12:25:06 -0700

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 // packages
 const express = require('express')
+const path = require('path')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
 const dbConnect = require('./utils/mongoDb')
@@ -23,7 +24,18 @@ app.use('/api/user', userRoutes)
 app.use('/api/post', postRoutes)
 app.use('/api/comments', commentRoutes)
 
+//Serve frontend
+if(process.env.NODE__ENV === 'production') {
+app.use(express.static(path.join(__dirname, '../frontend/build')))
 
+app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+    )
+  );
+} else {
+  app.get('/', (req, res) => res.send('Please set to production'));
+}
 
 
 app.listen(port, async () => {
